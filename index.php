bb2e6b0fbd22d6b19637921427351c5661686cbe
Pzsigmond2004 <p.zsigmond2004@gmail.com>
2026-05-09 07:55:51 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,33 +1,15 @@
-<table>
-    <caption>5. gyakorlat 1. feladat:<br>TÁBLÁZAT</caption>
-    <tr>
-        <th>Employee</th>
-        <th>Salary</th>
-        <th>Bonus</th>
-        <th>Supervisor</th>
-    </tr>
-    <tr>
-        <td>Stephen C. Cox</td>
-        <td>$300</td>
-        <td>$50</td>
-        <td>Bob</td>
-    </tr>
-    <tr>
-        <td>Josephin Tan</td>
-        <td>$150</td>
-        <td>-</td>
-        <td>Annie</td>
-    </tr>
-    <tr>
-        <td>Joyce Ming</td>
-        <td>$200</td>
-        <td>$35</td>
-        <td>Andy</td>
-    </tr>
-    <tr>
-        <td>James A. Pentel</td>
-        <td>$175</td>
-        <td>$25</td>
-        <td>Annie</td>
-    </tr>
-</table>
+<?php
+	include('./includes/config.inc.php');
+	$oldal = $_SERVER['QUERY_STRING'];
+	if ($oldal!="") {
+		if (isset($oldalak[$oldal]) && file_exists("./templates/pages/{$oldalak[$oldal]['fajl']}.tpl.php")) {
+			$keres = $oldalak[$oldal];
+		}
+		else { 
+			$keres = $hiba_oldal;
+			header("HTTP/1.0 404 Not Found");
+		}
+	}
+	else $keres = $oldalak['/'];
+	include('./templates/index.tpl.php'); 
+?>
